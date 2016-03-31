da3479d72052065c6e7556a20ff018a9f5d434cd
gtrujillop <gaston.trujillo.java@gmail.com>
2016-03-31 11:26:29 -0500

    Sessions: Add sessions to PackageSerializer.

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,5 +1,6 @@
 class Package < ActiveRecord::Base
   has_many :sessions
+  accepts_nested_attributes_for :sessions
   has_many :user_packages
   has_many :users, through: :user_packages
 
--- app/models/session.rb
+++ app/models/session.rb
@@ -8,4 +8,12 @@ class Session < ActiveRecord::Base
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  scope :by_package, -> (package_id) { where("package_id = ?", "#{package_id}") }
+  scope :by_user, -> (user_id) { joins(:user_session).where("user_id = ?", "#{package_id}") }
+
+  def self.find_by_package_and_user(package_id, user_id)
+    by_package(package_id).by_user(user_id)
+  end
+
 end
